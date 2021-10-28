86f94b6d70a4ff9e83ed3da8187d85ae7371f705
byun618 <byun618@gmail.com>
2021-10-29 08:07:36 +0900

    폰트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,15 @@ import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import IndexPage from './pages/IndexPage'
 import MarketPage from './pages/MarketPage'
-import styled from 'styled-components'
-
-const Helmet = styled.div`
-  position: fixed;
-  background-color: blue;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-`
 
 function App() {
   return (
-    // <Helmet>
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={IndexPage} />
         <Route path="/market" component={MarketPage} />
       </Switch>
     </BrowserRouter>
-    // </Helmet>
   )
 }
 
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,5 +1,26 @@
+import styled from 'styled-components'
+
+const Thin = styled.div`
+  font-weight: 100;
+`
+
+const Medium = styled.div`
+  font-weight: 500;
+`
+
+const Bold = styled.div`
+  font-weight: bold;
+`
+
 const IndexPage = () => {
-  return <div>index</div>
+  return (
+    <>
+      <Thin>index ㅁㄴㅇㅁㄴㅇ</Thin>
+      <div>123123asdas</div>
+      <Medium>asdasdasd ㅇㄴㅁㅇㅁㄴㅇ</Medium>
+      <Bold>aseqse123ㅁㄴㅇㅁㄴㅇ</Bold>
+    </>
+  )
 }
 
 export default IndexPage
